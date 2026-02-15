ba9d4325086f41a1f222c36de796a1bdecca1f41
Evgeniy Dimitriev <dimitriev_ev@mail.ru>
2026-02-15 19:31:39 +0500

    Extend Settings class with Google API credentials

--- app/core/config.py
+++ app/core/config.py
@@ -1,3 +1,4 @@
+from pydantic import EmailStr
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -8,6 +9,18 @@ class Settings(BaseSettings):
     description: str = 'Сервис для поддержки котиков'
     database_url: str = 'sqlite+aiosqlite:///./fastapi.db'
     secret_key: str = 'secret_key'
+
+    type: str | None = None
+    project_id: str | None = None
+    private_key_id: str | None = None
+    private_key: str | None = None
+    client_email: str | None = None
+    client_id: str | None = None
+    auth_uri: str | None = None
+    token_uri: str | None = None
+    auth_provider_x509_cert_url: str | None = None
+    client_x509_cert_url: str | None = None
+    email: EmailStr | None = None
 
     model_config = SettingsConfigDict(
         env_file='.env',
